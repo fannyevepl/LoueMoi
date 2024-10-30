1e5be6794185124f04b1006eecc66c3991133e04
Erwan Peres <eperes@jebidouille.com>
2024-10-29 22:22:41 -0400

    about us page and 404 redirection

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module LoueMoi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.exceptions_app = ->(env) { ErrorsController.action(:error404).call(env) }
   end
 end

d74626bbdc75b902130463a7598fdf47e3fd6157
Erwan Peres <eperes@jebidouille.com>
2024-10-29 23:04:17 -0400

    corrections mineures

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'about' => 'pages#about'
 
   resources :items do
     resources :reservations
   end
   resources :reservations
-  
-  # Route pour gérer les erreurs 404
-  match '*path', to: 'errors#error404', via: :all
 end

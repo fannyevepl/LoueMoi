0f41746fc5561a414dc0dd35e09ca8fce50c3e1a
Jean-Marcien Kablansi <jean.marcien@gmail.com>
2024-10-29 20:34:37 -0400

    Add routes for reservations and items

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :items do
+    resources :reservations
+  end
+  resources :reservations
 end

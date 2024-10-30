79c5c37375ee2814d17afbb0050aea6b6570b254
santiago velazquez <santiagovdiaz@outlook.com>
2024-10-30 15:42:42 -0400

    Added the ability to create a new reservation if user is signed in

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,30 @@
 class ReservationsController < ApplicationController
-  def new
-  end
+  before_action :set_item, only: [:new, :create]
 
-  def create
+  def new
+    @reservation = @item.reservations.build
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def create
+    @reservation = @item.reservations.build(reservation_params.merge(user: current_user, status: :pending))
+    if @reservation.save
+      redirect_to @reservation, notice: 'Your reservation is pending confirmation.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
   end
 end

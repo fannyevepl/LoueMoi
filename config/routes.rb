973110c11eac8b74b52988d29f490600a94179c5
santiago velazquez <santiagovdiaz@outlook.com>
2024-11-01 19:22:15 -0400

    added functionality to accept and cancel options

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'pages/about', as: 'about'
-  
+
   resources :users, only: [] do
     resources :items
   end
@@ -15,11 +15,15 @@ Rails.application.routes.draw do
     collection do
       get :reservations_for_my_items
     end
+    member do
+      patch :accept
+      patch :cancel
+    end
   end
-  
+
   # Route pour les erreurs 404
   match '/404', to: 'errors#not_found', via: :all
-  
+
   # Rediriger toutes les autres routes non trouvées vers la page 404
   match '*path', to: 'errors#not_found', via: :all
 end

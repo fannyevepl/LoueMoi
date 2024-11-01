6be3de0f2c5426f199df2bee7eb8fe2d257e37ae
Erwan Peres <eperes@jebidouille.com>
2024-11-01 13:52:16 -0400

    categories and category dropdown choice

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,21 @@
 class Item < ApplicationRecord
+  CATEGORY  = [
+    "Gardening Tools",
+    "Sports Equipment",
+    "DIY Tools",
+    "Camping Gear",
+    "Kitchen Appliances",
+    "Cleaning Equipment",
+    "Games and Toys",
+    "Party Supplies",
+    "Reading and Study Materials",
+    "Transportation Equipment"
+  ]
+  
   belongs_to :user
   has_many :reservations, dependent: :destroy
 
   validates :name, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :description, presence: true
 end

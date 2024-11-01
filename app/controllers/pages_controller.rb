404e258b0267dff6c2a3e3478e74cf8731575ea2
Erwan Peres <eperes@jebidouille.com>
2024-10-31 20:03:31 -0400

    pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @items = Item.all
+    @items = Item.all.includes(:user).order(:name)
+  end
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'pages/about', as: 'about'
 
   resources :items do
     resources :reservations, only: [:new, :create]
   end
   resources :reservations, only: [:index, :show, :destroy]
+  
+  # Route pour les erreurs 404
+  match '/404', to: 'errors#not_found', via: :all
+  
+  # Rediriger toutes les autres routes non trouvées vers la page 404
+  match '*path', to: 'errors#not_found', via: :all
 end

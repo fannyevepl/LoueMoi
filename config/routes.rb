7e4489efb1536aeb0b442e446dd7447f18aad636
Jean-Marcien Kablansi <jean.marcien@gmail.com>
2024-11-02 00:21:43 -0400

    To save to my branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'pages/about', as: 'about'
-  
+
   resources :users, only: [] do
     resources :items
   end
@@ -11,15 +11,17 @@ Rails.application.routes.draw do
     resources :reservations, only: [:new, :create]
   end
 
+  get 'search_items', to: 'items#search'
+
   resources :reservations, only: [:index, :show, :destroy] do
     collection do
       get :reservations_for_my_items
     end
   end
-  
+
   # Route pour les erreurs 404
   match '/404', to: 'errors#not_found', via: :all
-  
+
   # Rediriger toutes les autres routes non trouvées vers la page 404
   match '*path', to: 'errors#not_found', via: :all
 end

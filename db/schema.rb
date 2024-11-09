7950e18dbc7e9b36469c55c718aa9afc8a62516b
Erwan Peres <eperes@jebidouille.com>
2024-11-09 15:30:06 -0500

    remove old migration

--- db/schema.rb
+++ db/schema.rb
@@ -79,10 +79,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_08_024543) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

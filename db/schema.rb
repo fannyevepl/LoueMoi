269530fe5735f1e4621a0fa32b71ddd0233d8e3e
FannyEve PotvinLemay <fannyevepl@gmail.com>
2024-11-09 12:11:31 -0500

    adding profil picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_07_144307) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_08_024543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,9 +49,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_07_144307) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.string "address"
     t.index ["category"], name: "index_items_on_category"
     t.index ["user_id", "name"], name: "index_items_on_user_id_and_name"
     t.index ["user_id"], name: "index_items_on_user_id"
